e4ea8136c24bd708de752388a6a1fc9a410bbf4b
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-07-20 10:50:38 +0200

    Update fillable

--- app/Models/BotSetting.php
+++ app/Models/BotSetting.php
@@ -13,15 +13,15 @@ class BotSetting extends Model
     public $timestamps = false;
 
     protected $fillable = [
-        'message_edited',
-        'message_deleted',
-        'message_reaction',
-        'voice_join_leave',
-        'voice_change',
-        'member_join_leave',
-        'threads',
-        'mod_ban_unban',
-        'member_nickname',
+        'sw_message_edited',
+        'sw_message_deleted',
+        'sw_message_reaction',
+        'sw_vc_join_leave',
+        'sw_vc_change',
+        'sw_join_leave',
+        'sw_threads',
+        'sw_ban_unban',
+        'sw_nickname_change',
         'xp_messages',
         'xp_voicechat',                                                                
     ];
