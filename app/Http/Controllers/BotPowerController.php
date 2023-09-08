23459022f6bc9d4cd222672efe53e584c6738ada
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-09-08 18:52:13 +0200

    Update

--- app/Http/Controllers/BotPowerController.php
+++ app/Http/Controllers/BotPowerController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\BotPower;
 
 class BotPowerController extends Controller
 {
     public function getPower()
     {
+        $TOKEN = env('PTERODACTYL_TOKEN');
+        $ch = curl_init('https://bothostmanager.kelvincodes.nl/api/application/servers/2');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , "Authorization: Bearer $TOKEN"));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $ptero = json_decode(curl_exec($ch), true);
+
         return view('bot.power');
     }
 }
